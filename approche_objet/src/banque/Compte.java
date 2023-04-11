15c25a3ebb80587d6659ebcfed99952f6ae89441
Nicolas VINCKIER <nicolas.vinckier05@gmail.com>
2023-04-11 17:06:34 +0200

    Banque

---
+++ approche_objet/src/banque/Compte.java
@@ -0,0 +1,27 @@
+package banque;
+
+public class Compte {
+	int numero_de_compte;
+	float solde;
+
+	// Constructeur
+	public Compte(int numero_de_compte, float solde) {
+		this.numero_de_compte = numero_de_compte;
+		this.solde = solde;
+	};
+
+	// Avoir le numéro du compte
+	public int getNumeroDeCompte() {
+		return numero_de_compte;
+	};
+
+	// Avoir le solde du compte
+	public float getSolde() {
+		return solde;
+	};
+
+	// Return
+	public String toString() {
+		return ("Numéro du compte : " + numero_de_compte + " | Solde : " + solde +" €");
+	};
+};
---
+++ approche_objet/src/banque/TestBanque.java
@@ -0,0 +1,13 @@
+package banque;
+
+import banque.*;
+
+public class TestBanque {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Compte compte1 = new Compte(45875, 14.23f);
+		System.out.println(compte1.toString());
+	};
+
+};
