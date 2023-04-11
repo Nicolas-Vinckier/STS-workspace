49e3cdd1d9b18841bbd33139a1291673edf51166
Nicolas VINCKIER <nicolas.vinckier05@gmail.com>
2023-04-11 17:09:22 +0200

    Update TestBanque.java

--- approche_objet/src/banque/TestBanque.java
+++ approche_objet/src/banque/TestBanque.java
@@ -1,12 +1,13 @@
 package banque;
 
-import banque.*;
+// import banque.*;
 
 public class TestBanque {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Compte compte1 = new Compte(45875, 14.23f);
+		Compte compte1 = new Compte(45875, 14.23f); // ne pas oublier le "f" - pour float
+		System.out.println(compte1);
 		System.out.println(compte1.toString());
 	};
 
