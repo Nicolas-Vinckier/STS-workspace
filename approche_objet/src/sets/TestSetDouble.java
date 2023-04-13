b340af0b3a25b0e3069418492734055a41af63e6
Nicolas VINCKIER <nicolas.vinckier05@gmail.com>
2023-04-13 16:30:47 +0200

    TestSetDouble

---
+++ approche_objet/src/sets/TestSetDouble.java
@@ -0,0 +1,31 @@
+package sets;
+
+public class TestSetDouble {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double[] HashSet = { 1.5, 8.25, -7.32, 13.3, -12.45, 48.5, 0.01 };
+
+		double JeanMax = 0;
+
+		for (int i = 0; i < HashSet.length; i++) {
+			System.out.println(HashSet[i]);
+			if (HashSet[i] > JeanMax) {
+				JeanMax = HashSet[i];
+			}
+		}
+		System.out.println("Le plus gros est " + JeanMax);
+
+		double JeanMin = Integer.MAX_VALUE;
+
+		for (int i = 0; i < HashSet.length; i++) {
+			if (HashSet[i] < JeanMin) {
+				JeanMin = HashSet[i];
+			}
+		}
+		System.out.println("Le plus faible est " + JeanMin);
+
+		// Supprimé le plus petit 
+
+	}
+}
