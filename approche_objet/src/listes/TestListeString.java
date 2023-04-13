223b8eee664e1dd8b2104937829532c3b4107527
Nicolas VINCKIER <nicolas.vinckier05@gmail.com>
2023-04-13 14:45:06 +0200

    Update TestListeString.java

--- approche_objet/src/listes/TestListeString.java
+++ approche_objet/src/listes/TestListeString.java
@@ -22,25 +22,39 @@ public class TestListeString {
 		Villeagois.add("Tarbes");
 		System.out.println(Villeagois);
 
-		int bigmot = 0; 
+		// int bigmot = 0;
+		String bigmot = "";
 
-		for (int i = 1; i < Villeagois.size();) {
+		// for (int i = 1; i < Villeagois.size();) {
+		// String mot = Villeagois.get(i);
+		// int tailledumot = mot.length();
+		// System.out.println("La taille du mot " + Villeagois.get(i) + " est de " +
+		// tailledumot + " caractères");
+
+		// while (true) {
+		// if (tailledumot > bigmot) {
+		// bigmot = tailledumot;
+		// }
+		// if (tailledumot < bigmot) {
+		// break;
+		// }
+		// }
+		// }
+
+		// Suppretion de la boucle infinie #While(true)
+		for (int i = 0; i < Villeagois.size(); i++) {
 			String mot = Villeagois.get(i);
 			int tailledumot = mot.length();
 			System.out.println("La taille du mot " + Villeagois.get(i) + " est de " + tailledumot + " caractères");
 
-			while (true) {
-				if (tailledumot > bigmot) {
-					bigmot = tailledumot;
-				}
-				if (tailledumot < bigmot) {
-					continue;
-				}
+			if (tailledumot > bigmot.length()) { // Comparer la taille du mot avec la taille du plus gros mot actuel
+				bigmot = mot; // Mettre à jour le plus gros mot si nécessaire
 			}
 		}
+		System.out.println("");
+		System.out.println("Le plus gros mot est : " + bigmot + " qui fait " + bigmot.length() + " caractères");
 
-
-		// Faire en sorte de supprimer les villes qui commence par N 
+		// Faire en sorte de supprimer les villes qui commence par N
 
 	}
 }
