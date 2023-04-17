e81ce0f4aba48caef05a3a3eb634e28cd84edb57
Nicolas VINCKIER <nicolas.vinckier05@gmail.com>
2023-04-17 16:40:19 +0200

    Patch LectureFichier

--- approche_objet/src/fichier/LectureFichier.java
+++ approche_objet/src/fichier/LectureFichier.java
@@ -15,14 +15,14 @@ public class LectureFichier {
 		public String nom;
 		public String codeDepartement;
 		public String nomRegion;
-		public int populationTotale;
+		public String populationTotale;
 
 		// Constructeur
-		public Ville(String nom, String codeDepartement, String nomRegion, int populationTotale) {
+		public Ville(String nom, String codeDepartement, String nomRegion, String populationTotale2) {
 			this.nom = nom;
 			this.codeDepartement = codeDepartement;
 			this.nomRegion = nomRegion;
-			this.populationTotale = populationTotale;
+			this.populationTotale = populationTotale2;
 		}
 
 		// Getters
@@ -38,19 +38,17 @@ public class LectureFichier {
 			return nomRegion;
 		}
 
-		public int getPopulationTotale() {
+		public String getPopulationTotale() {
 			return populationTotale;
 		}
 	}
 
 	public static void main(String[] args) throws IOException {
+		// Path principale
 		Path path = Paths.get(
 				"C:\\Users\\mrcan\\OneDrive\\Bureau\\STS-workspace\\approche_objet\\src\\fichier\\recensement.csv");
-		System.out.println(path.getRoot());
-		System.out.println(path.getParent());
-		System.out.println(path);
 
-		List<String> lines = Files.readAllLines(path, Charset.defaultCharset());
+		List<String> lines = Files.readAllLines(path, Charset.defaultCharset()); // Lire toutes les lignes
 
 		// Création de la liste d'instances de la classe Ville
 		ArrayList<Ville> villes = new ArrayList<Ville>();
@@ -64,7 +62,7 @@ public class LectureFichier {
 			String nom = tokens[0];
 			String codeDepartement = tokens[1];
 			String nomRegion = tokens[2];
-			int populationTotale = Integer.parseInt(tokens[3]);
+			String populationTotale = tokens[3];
 
 			// Création d'une instance de la classe Ville
 			Ville ville = new Ville(nom, codeDepartement, nomRegion, populationTotale);
@@ -74,14 +72,16 @@ public class LectureFichier {
 		}
 
 		// Affichage des villes
-		for (Ville ville : villes) {
-			System.out.println("Nom: " + ville.getNom());
-			System.out.println("Code département: " + ville.getCodeDepartement());
-			System.out.println("Nom région: " + ville.getNomRegion());
-			System.out.println("Population totale: " + ville.getPopulationTotale());
-			System.out.println("-----------");
-		}
+		// for (Ville ville : villes) {
+		// System.out.println("Nom: " + ville.getNom());
+		// System.out.println("Code département: " + ville.getCodeDepartement());
+		// System.out.println("Nom région: " + ville.getNomRegion());
+		// System.out.println("Population totale: " + ville.getPopulationTotale());
+		// System.out.println("-----------");
+		// }
 
+
+		
 	}
 
 }
