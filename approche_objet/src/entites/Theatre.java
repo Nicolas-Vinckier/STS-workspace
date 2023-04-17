4d5ee3f1f6c8933bfa781f35315d7be6327bd43f
Nicolas VINCKIER <nicolas.vinckier05@gmail.com>
2023-04-17 09:50:55 +0200

    Theatre

---
+++ approche_objet/src/entites/Theatre.java
@@ -0,0 +1,23 @@
+package entites;
+
+public class Theatre {
+	public String nom;
+	public int capMax;
+	public int clientIns;
+	public float recete;
+
+	public Object inscrire(int capMax, int clientIns){
+		if (clientIns < capMax){
+			System.out.println("Inscription ok");
+		} else {
+			System.out.println("Inscription impossible");
+		}
+		return clientIns;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
