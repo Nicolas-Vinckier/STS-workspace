48f823be5bda56e2ac0ea81a63a9048432b15b3a
Nicolas VINCKIER <nicolas.vinckier05@gmail.com>
2023-04-19 15:17:50 +0200

    Update TestEquals.java

--- approche_objet/src/listes/TestEquals.java
+++ approche_objet/src/listes/TestEquals.java
@@ -4,6 +4,20 @@ public class TestEquals {
 
 	public static void main(String[] args) {
 
+		Ville Villeagois1 = new Ville("Marseille", 1000);
+		Ville Villeagois2 = new Ville("Paris", 1000);
+
+		if (Villeagois1.getNbHabitants().equals(Villeagois2.getNbHabitants())) {
+			System.out.println("Les deux villes sont identiques.");
+		} else {
+			System.out.println("Les deux villes sont différentes.");
+		}
+
+		// if (Villeagois1.getNbHabitants() == Villeagois2.getNbHabitants()) {
+		// System.out.println("Les deux villes sont identiques.");
+		// } else {
+		// System.out.println("Les deux villes sont différentes.");
+		// }
 	}
 
 }
